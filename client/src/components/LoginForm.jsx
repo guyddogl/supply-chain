1594b8c37ea6aa6edeee01aa18b93b19a9fe6a33
guyddogl <guyddogl@gmail.com>
2022-11-19 15:04:09 -0300

    Refatora identação do código

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/img/logox64.png';
 import AppContext from '../context/AppContext';
@@ -6,7 +6,7 @@ import Axios from '../services/axios';
 import { showToast } from '../services/toastr';
 
 export default function Login() {
-  const INITIAL_STATE = {username: '', password: ''};
+  const INITIAL_STATE = { username: '', password: '' };
 
   const [inputsLoginForm, setInputsLoginForm] = useState(INITIAL_STATE);
 
@@ -34,81 +34,84 @@ export default function Login() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setIsLoading(true);
-    await Axios.get(`/usuarios/${inputsLoginForm.username}`, { body: { password: inputsLoginForm.password}})
-    .then((response) => {
-      if (response.status === 200) {
-        setCurrentUser(response.data);
-        setIsUserLoggedIn(true);
-        showToast('success', 'Login realizado com sucesso');
+    await Axios.get(
+      `/usuarios/${inputsLoginForm.username}`,
+      { body: { password: inputsLoginForm.password } },
+    )
+      .then((response) => {
+        if (response.status === 200) {
+          setCurrentUser(response.data);
+          setIsUserLoggedIn(true);
+          showToast('success', 'Login realizado com sucesso');
+          setIsLoading(false);
+          navigate('/');
+        }
+      })
+      .catch((err) => {
+        showToast('error', err.response.data);
         setIsLoading(false);
-        navigate('/');
-      }
-    })
-    .catch((err) => {
-      showToast('error', err.response.data);
-      setIsLoading(false);
-    });
-  }
+      });
+  };
 
   return (
-        <form
-          onSubmit={handleSubmit}
-          className="col-10 col-md-6 col-lg-4 rounded-3 p-4 shadow-sm"
-          style={{ maxWidth: '350px', background: 'white' }}
-        >
-          <img
-            src={logo}
-            className="img-fluid mx-auto d-block mb-4"
-            alt="Supply Chain"
-            style={{ maxWidth: '80px' }}
-          />
-          <div className="input-group mb-3">
-            <span className="input-group-text text-secondary">
-              <i className="fa-solid fa-envelope" />
-            </span>
-            <input
-              type="username"
-              name="username"
-              value={inputsLoginForm.username}
-              placeholder="username"
-              className="form-control"
-              onChange={handleFormInputs}
-              required
-            />
-          </div>
-          <div className="input-group mb-3">
-            <span className="input-group-text text-secondary">
-              <i className="fa-solid fa-key" />
-            </span>
-            <input
-              type="password"
-              name="password"
-              value={inputsLoginForm.password}
-              placeholder="password"
-              className="form-control"
-              onChange={handleFormInputs}
-              required
-            />
-          </div>
-          {isLoading
-            ? (
-              <button className="btn btn-primary d-block mx-auto mt-4 form-control" type="button" disabled>
-                <span className="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true" />
-                Acessando...
-              </button>
-            )
-            : (
-              <button
-                type="submit"
-                className={
+    <form
+      onSubmit={handleSubmit}
+      className="col-10 col-md-6 col-lg-4 rounded-3 p-4 shadow-sm"
+      style={{ maxWidth: '350px', background: 'white' }}
+    >
+      <img
+        src={logo}
+        className="img-fluid mx-auto d-block mb-4"
+        alt="Supply Chain"
+        style={{ maxWidth: '80px' }}
+      />
+      <div className="input-group mb-3">
+        <span className="input-group-text text-secondary">
+          <i className="fa-solid fa-envelope" />
+        </span>
+        <input
+          type="username"
+          name="username"
+          value={inputsLoginForm.username}
+          placeholder="username"
+          className="form-control"
+          onChange={handleFormInputs}
+          required
+        />
+      </div>
+      <div className="input-group mb-3">
+        <span className="input-group-text text-secondary">
+          <i className="fa-solid fa-key" />
+        </span>
+        <input
+          type="password"
+          name="password"
+          value={inputsLoginForm.password}
+          placeholder="password"
+          className="form-control"
+          onChange={handleFormInputs}
+          required
+        />
+      </div>
+      {isLoading
+        ? (
+          <button className="btn btn-primary d-block mx-auto mt-4 form-control" type="button" disabled>
+            <span className="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true" />
+            Acessando...
+          </button>
+        )
+        : (
+          <button
+            type="submit"
+            className={
           `btn btn-md btn-primary d-block mx-auto mt-4 form-control
           ${isLoginButtonDisabled && 'btn-secondary disabled'}`
         }
-                disabled={isLoginButtonDisabled}
-              >
-                Acessar
-              </button>
-            )}
-        </form>
-  )
+            disabled={isLoginButtonDisabled}
+          >
+            Acessar
+          </button>
+        )}
+    </form>
+  );
 }
