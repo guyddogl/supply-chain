2cbe82af96cf91005be334220beea7da947333d9
guyddogl <guyddogl@gmail.com>
2022-11-19 20:53:28 -0300

    Adiciona rotas fabricantes e mercadorias

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,15 @@ const app = express();
 const cors = require('cors');
 const { validateAuthorization } = require('./middlewares/validateAuthorization');
 const usersRoutes = require('./routes/usersRoutes');
+const fabricantesRoutes = require('./routes/fabricantesRoutes');
+const mercadoriasRoutes = require('./routes/mercadoriasRoutes');
 
 app.use(cors());
 app.use(express.json());
 app.use(validateAuthorization);
 
 app.use('/usuarios', usersRoutes);
+app.use('/fabricantes', fabricantesRoutes);
+app.use('/mercadorias', mercadoriasRoutes);
 
 module.exports = app;
