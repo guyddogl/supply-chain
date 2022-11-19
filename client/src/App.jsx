7ec2682f91204374a679ae9f7866ec920461e9b9
guyddogl <guyddogl@gmail.com>
2022-11-19 18:27:08 -0300

    Importa a página fabricantes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import 'toastr/build/toastr.min.css';
 import ProtectedRoutes from './components/ProtectedRoutes';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Fabricantes from './pages/Fabricantes';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<ProtectedRoutes component={Home} />} />
+      <Route path="/fabricantes" element={<ProtectedRoutes component={Fabricantes} />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
