469e5fdcc0381f59efa742efccebf34b8ad9407e
guyddogl <guyddogl@gmail.com>
2022-11-19 15:34:23 -0300

    Adiciona NavBar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
+import NavBar from '../components/NavBar';
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <NavBar />
+  );
 }
