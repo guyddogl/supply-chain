8e97e9a5059ecfdb5d7de4f17dcd607b063092e1
guyddogl <guyddogl@gmail.com>
2022-11-19 13:19:12 -0300

    Cria o App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './assets/css/global.css';
+import ProtectedRoutes from './components/ProtectedRoutes';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<ProtectedRoutes component={Home} />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
