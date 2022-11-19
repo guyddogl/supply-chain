73a31c665206d7b7306424574133411f8bbe5a3d
guyddogl <guyddogl@gmail.com>
2022-11-19 14:51:11 -0300

    Adiciona middleware para validar o token das requisições

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { validateAuthorization } = require('./middlewares/validateAuthorization');
 const usersRoutes = require('./routes/usersRoutes');
 
 app.use(cors());
 app.use(express.json());
+app.use(validateAuthorization);
 
 app.use('/usuarios', usersRoutes);
 
