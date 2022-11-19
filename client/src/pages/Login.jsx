da75baa1cfa523448cec3df055b298ea585f7e43
guyddogl <guyddogl@gmail.com>
2022-11-19 18:29:32 -0300

    Modifica a estrutura html

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import LoginForm from '../components/LoginForm';
 
 export default function Login() {
-
   return (
     <main style={{ background: '#f1f1f1' }}>
-    <section className="container-fluid">
-      <div className="row justify-content-center align-items-center h100">
-        <LoginForm />
-      </div>
-    </section>
-  </main>
-  )
+      <section className="container-fluid">
+        <div className="row justify-content-center align-items-center h100">
+          <LoginForm />
+        </div>
+      </section>
+    </main>
+  );
 }
