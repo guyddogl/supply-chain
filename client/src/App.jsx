2a6d037bbb29aedf6a5bf92792698ee22f2a7b92
guyddogl <guyddogl@gmail.com>
2022-11-19 14:43:50 -0300

    Importa toastr css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './assets/css/global.css';
+import 'toastr/build/toastr.min.css';
 import ProtectedRoutes from './components/ProtectedRoutes';
 import Login from './pages/Login';
 import Home from './pages/Home';
