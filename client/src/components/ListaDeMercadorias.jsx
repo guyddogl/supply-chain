0f0a73022d0580cbba74276787712642ca7248df
guyddogl <guyddogl@gmail.com>
2022-11-19 21:44:08 -0300

    Cria componente para exibir mercadorias

--- client/src/components/ListaDeMercadorias.jsx
+++ client/src/components/ListaDeMercadorias.jsx
@@ -6,9 +6,15 @@ import AppContext from '../context/AppContext';
 export default function ListaDeMercadorias() {
   const [mercadorias, setMercadorias] = useState([]);
 
+  const [fabricantes, setFabricantes] = useState([]);
+
   const [isLoading, setIsLoading] = useState(false);
 
   const { update } = useContext(AppContext);
+
+  const [inputSearch, setInputSearch] = useState('');
+
+  const [selectSearch, setSelectSearch] = useState('');
 
   useEffect(() => {
     setIsLoading(true);
@@ -25,7 +31,29 @@ export default function ListaDeMercadorias() {
       setIsLoading(false);
     };
     getAllMercadorias();
+    const getAllFabricantes = async () => {
+      await Axios.get('fabricantes')
+        .then((response) => {
+          setFabricantes(response.data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          showToast('error', err.response.data);
+          setIsLoading(false);
+        });
+      setIsLoading(false);
+    };
+    getAllFabricantes();
   }, [update]);
+
+  const removeFilters = () => {
+    setInputSearch('');
+    setSelectSearch('Todos');
+  };
+
+  const lista = mercadorias
+    .filter((mercadoria) => ((selectSearch === 'Todos') ? mercadoria.fabricante : mercadoria.fabricante.includes(selectSearch)))
+    .filter((mercadoria) => mercadoria.nome.toLowerCase().includes(inputSearch.toLowerCase()));
 
   return (
     <table className="table table-striped caption-top">
@@ -37,6 +65,54 @@ export default function ListaDeMercadorias() {
             <span className="visually-hidden">Loading...</span>
           </div>
         )}
+      </caption>
+      <caption>
+        <div className="row justify-content-start">
+          <div className="col-6 col-lg-4">
+            <label htmlFor="nome" className="form-label" style={{ width: '100%' }}>
+              <div className="input-group">
+                <span className="input-group-text text-secondary">
+                  <i className="fas fa-search" />
+                </span>
+                <input
+                  type="text"
+                  name="nome"
+                  value={inputSearch}
+                  placeholder="Pesquisar por nome"
+                  className="form-control"
+                  onChange={(e) => setInputSearch(e.target.value)}
+                />
+              </div>
+            </label>
+          </div>
+          <div className="col-6 col-lg-3">
+            <label htmlFor="fabricante" className="form-label" style={{ width: '100%' }}>
+              <select
+                name="fabricante"
+                className="form-control form-select"
+                onChange={(e) => setSelectSearch(e.target.value)}
+                value={selectSearch}
+              >
+                <option value="Todos">
+                  Todos os Fabricantes
+                </option>
+                {fabricantes.map((fabricante) => (
+                  <option key={fabricante.id} value={fabricante.nome}>
+                    {fabricante.nome}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <div className="col-6 col-lg-3">
+            <div className="input-group">
+              <button type="button" name="filtro" className="btn btn-md btn-outline-secondary" onClick={removeFilters}>
+                <i className="fa-solid fa-filter-circle-xmark me-2" />
+                Remover filtros
+              </button>
+            </div>
+          </div>
+        </div>
       </caption>
       <thead>
         <tr>
@@ -61,7 +137,7 @@ export default function ListaDeMercadorias() {
         </tr>
       </thead>
       <tbody className="table-group-divider">
-        {mercadorias.length >= 1 && mercadorias.map((mercadoria) => (
+        {lista.length >= 1 && lista.map((mercadoria) => (
           <tr key={mercadoria.nome}>
             <td>
               {mercadoria.nome}
