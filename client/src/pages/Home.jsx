ad406ec6582824dffd1c0b9d11f6b2e4533efa7f
guyddogl <guyddogl@gmail.com>
2022-11-21 12:47:54 -0300

    Adiciona a lista de entradas na home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import EntradaMercadoria from '../components/EntradaMercadoria';
+import ListaDeEntradas from '../components/ListaDeEntradas';
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <section className="container">
-        <EntradaMercadoria />
+        <div className="row">
+          <div className="col-12 col-lg-6">
+            <EntradaMercadoria />
+            <ListaDeEntradas />
+          </div>
+          <div className="col-12 col-lg-6">
+            <EntradaMercadoria />
+            <ListaDeEntradas />
+          </div>
+        </div>
       </section>
     </>
   );
