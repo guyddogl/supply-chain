7a79eab6e73e240afc046d9916c620aea372b882
guyddogl <guyddogl@gmail.com>
2022-11-21 11:16:20 -0300

    Adiciona botão para cadastrar entrada de mercadorias

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import EntradaMercadoria from '../components/EntradaMercadoria';
 
 export default function Home() {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <section className="container">
+        <EntradaMercadoria />
+      </section>
+    </>
   );
 }
