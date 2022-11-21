b3c9efe8387cba5360cbada924ab184de38fd7b7
guyddogl <guyddogl@gmail.com>
2022-11-21 11:20:02 -0300

    Altera o nome do arquivo

--- server/src/services/mercadoriasServices.js
+++ server/src/services/mercadoriasServices.js
@@ -1,12 +1,12 @@
-const mercadoriaModels = require('../models/mercadoriaModels');
+const mercadoriasModels = require('../models/mercadoriasModels');
 
 const getAllMercadorias = async (_req, res) => {
-  const mercadorias = await mercadoriaModels.getAllMercadorias();
+  const mercadorias = await mercadoriasModels.getAllMercadorias();
   return { status: 200, mercadorias };
 };
 
 const addNewMercadoria = async (nome, registro, fabricante, tipo, descricao, criado_por, atualizado_por) => {
-  const result = await mercadoriaModels.addNewMercadoria(nome, registro, fabricante, tipo, descricao, criado_por, atualizado_por);
+  const result = await mercadoriasModels.addNewMercadoria(nome, registro, fabricante, tipo, descricao, criado_por, atualizado_por);
   if (result.affectedRows === 0) {
     return { status: 400, message: "Ocorreu um erro. Tente novamente." }
   }
