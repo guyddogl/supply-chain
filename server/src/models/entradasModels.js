5c0160544db691c903e72167673dc30af19d443e
guyddogl <guyddogl@gmail.com>
2022-11-21 12:47:34 -0300

    Ordena o retorno de getAllEntradas para DESC

--- server/src/models/entradasModels.js
+++ server/src/models/entradasModels.js
@@ -3,7 +3,8 @@ const connection = require('../database/connection');
 const getAllEntradas = async (_req, _res) => {
   const query = `SELECT entradas.id, mercadorias.nome AS mercadoria, entradas.quantidade, entradas.local, entradas.data_hora
   FROM entradas, mercadorias 
-  WHERE entradas.mercadoria = mercadorias.id`;
+  WHERE entradas.mercadoria = mercadorias.id
+  ORDER BY data_hora DESC`;
   const [entradas] = await connection.execute(query);
   return entradas;
 };
