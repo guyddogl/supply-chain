725959a4409433da6edb81f91ec5ae2c8183c532
guyddogl <guyddogl@gmail.com>
2022-11-21 18:03:02 -0300

    Cria as funções getInitalDate e getFinalDate

--- client/src/utils/dateTime.js
+++ client/src/utils/dateTime.js
@@ -4,11 +4,17 @@ export const formatDateTime = (date, format = 'dd/MM/yy HH:mm') => DateTime.from
   .setLocale('pt-br')
   .toFormat(format);
 
-export const getCurrentDate = (locale = 'BR') => {
+export const getInitialDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1) < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+  return `${year}-${month}-01T00:00`;
+};
+
+export const getFinalDate = () => {
   const date = new Date();
   const year = date.getFullYear();
   const month = (date.getMonth() + 1) < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
   const day = date.getDate();
-  if (locale === 'BR') return `${day}/${month}/${year}`;
-  return `${year}/${month}/${day}`;
+  return `${year}-${month}-${day}T23:59`;
 };
