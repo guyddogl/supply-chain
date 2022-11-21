29f55c8c094ba75822ef44c6fffc7f8e7ec9696a
guyddogl <guyddogl@gmail.com>
2022-11-21 14:03:21 -0300

    Adiciona componentes para adicionar e listar saídas na home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import NavBar from '../components/NavBar';
 import EntradaMercadoria from '../components/EntradaMercadoria';
 import ListaDeEntradas from '../components/ListaDeEntradas';
+import SaidaMercadoria from '../components/SaidaMercadoria';
+import ListaDeSaidas from '../components/ListaDeSaidas';
 
 export default function Home() {
   return (
@@ -14,8 +16,8 @@ export default function Home() {
             <ListaDeEntradas />
           </div>
           <div className="col-12 col-lg-6">
-            <EntradaMercadoria />
-            <ListaDeEntradas />
+            <SaidaMercadoria />
+            <ListaDeSaidas />
           </div>
         </div>
       </section>
