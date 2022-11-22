0722e5803c0f27cd1afbd938b895833f3820b4c5
guyddogl <guyddogl@gmail.com>
2022-11-22 10:24:21 -0300

    Modifica a posição da notificação

--- client/src/services/toastr.js
+++ client/src/services/toastr.js
@@ -5,7 +5,7 @@ const toastTopRight = {
   debug: false,
   newestOnTop: false,
   progressBar: true,
-  positionClass: 'toast-top-right',
+  positionClass: 'toast-top-center',
   preventDuplicates: false,
   onclick: null,
   showDuration: '300',
